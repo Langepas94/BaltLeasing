d74cf3974c3fff86cf7f708a077242cccc53c9a3
Artem <artyom9494@yandex.ru>
2023-12-11 15:25:30 +0300

    clean project

--- app/src/main/java/com/example/baltleasing/MainActivity.kt
+++ app/src/main/java/com/example/baltleasing/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.baltleasing
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
