53657076dd4c3bd981adbf42c68b4fca00d6abdc
Artem <artyom9494@yandex.ru>
2023-12-13 10:44:14 +0300

    Refactor project structure

--- app/src/main/java/com/example.baltleasing/destinations/ServicesScreen/ServicesFragment.kt
+++ app/src/main/java/com/example.baltleasing/destinations/ServicesScreen/ServicesFragment.kt
@@ -13,16 +13,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [UslugiFragment.newInstance] factory method to
+ * Use the [ServicesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class UslugiFragment : Fragment() {
+class ServicesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_uslugi, container, false)
+        return inflater.inflate(R.layout.fragment_services, container, false)
     }
 }
